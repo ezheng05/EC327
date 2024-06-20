14837a47f0346b39c61223eb4161b216f0a6827d
Prashastp25 <86373449+Prashastp25@users.noreply.github.com>
2024-06-20 14:53:53 -0400

    Mini Game
    This is one the of the two mini-games in our project

---
+++ BubblePopper/bigger.cpp
@@ -0,0 +1,76 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <cstdlib> // For std::rand() and std::srand()
+#include <ctime>   // For std::time()
+#include <string>
+#include <iostream>
+#include <SFML/Audio.hpp>
+using sf::Mouse;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "My window");
+
+    sf::Texture backgroundTexture;
+    if(!backgroundTexture.loadFromFile("./assets/background.jpg")){
+        return -1;
+    }
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("./assets/bubblepop.mp3"))
+        return -1;  
+
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+
+    sf::Sprite background(backgroundTexture);
+    sf::Vector2 windowSize = window.getSize();
+    sf::Vector2 textureSize = backgroundTexture.getSize();
+        background.setScale(
+        (float)(windowSize.x) / textureSize.x, 
+        (float)(windowSize.y) / textureSize.y
+    );
+    sf::Texture bubbleTexture;
+    if(!bubbleTexture.loadFromFile("./assets/bubble1.png")){
+        return -1;
+    }
+    bubbleTexture.setSmooth(true);
+    sf::Sprite bubble(bubbleTexture);
+    bubble.setScale(0.05f, 0.05f);
+
+    std::srand(std::time(NULL));
+
+    float x = (float)(std::rand() % (windowSize.x - (int)(bubble.getGlobalBounds().width)));
+    float y = (float)(std::rand() % (windowSize.y - (int)(bubble.getGlobalBounds().height)));
+    bubble.setPosition(x, y);
+    
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+            window.clear();
+        auto mouse_pos = sf::Mouse::getPosition(window); // Mouse position relative to the window
+        auto translated_pos = window.mapPixelToCoords(mouse_pos); // Mouse position translated into world coordinates
+        if(bubble.getGlobalBounds().contains(translated_pos)){ // Rectangle-contains-point check
+            // Mouse is inside the sprite.
+                float x = (float)(std::rand() % (windowSize.x - (int)(bubble.getGlobalBounds().width)));
+                float y = (float)(std::rand() % (windowSize.y - (int)(bubble.getGlobalBounds().height)));
+                    sound.play();
+                bubble.setPosition(x, y);
+            }
+        // Draw the background
+    window.draw(background);
+    window.draw(bubble);
+    window.display();
+
+    }        
+
+    return 0;
+}
