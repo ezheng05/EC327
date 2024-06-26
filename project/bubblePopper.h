020ad6a182552b5a77f3abf724ad3222d759fbe0
Prashastp25 <86373449+Prashastp25@users.noreply.github.com>
2024-06-26 14:43:22 -0400

    Update bubblePopper.h

--- project/bubblePopper.h
+++ project/bubblePopper.h
@@ -1,79 +1,147 @@
-// g++ --std=c++17 bubblePopper.cpp -I/opt/homebrew/Cellar/sfml/2.6.1/include -o app -L/opt/homebrew/Cellar/sfml/2.6.1/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
-#include <cstdlib> // For std::rand() and std::srand()
-#include <ctime>   // For std::time()
+#include <cstdlib>
+#include <ctime>
 #include <string>
-#include <iostream>
-#include <SFML/Audio.hpp>
+#include<iostream>
 
-using sf::Mouse;
-
-int bubblePopper() {
-    sf::RenderWindow window(sf::VideoMode(1200, 800), "Bubble Popper");
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Bee Game");
 
     sf::Texture backgroundTexture;
-    if(!backgroundTexture.loadFromFile("bubblePopper/background.jpg")){
-        return -1;
+    if (!backgroundTexture.loadFromFile("./assets/background.jpg")) {
+        return -1; // Error loading background image
     }
-    sf::SoundBuffer buffer;
-    if (!buffer.loadFromFile("bubblePopper/bubblepop.mp3"))
-        return -1;  
-
-    sf::Sound sound;
-    sound.setBuffer(buffer);
-
     sf::Sprite background(backgroundTexture);
+
     sf::Vector2 windowSize = window.getSize();
     sf::Vector2 textureSize = backgroundTexture.getSize();
-        background.setScale(
+
+//propotionally increase the scale of texture
+    background.setScale(
         (float)(windowSize.x) / textureSize.x, 
         (float)(windowSize.y) / textureSize.y
     );
-    sf::Texture bubbleTexture;
-    if(!bubbleTexture.loadFromFile("bubblePopper/bubble1.png")){
+
+    sf::Texture playerTexture;
+    if (!playerTexture.loadFromFile("./assets/player.png")) {
+        return -1; 
+    }
+    sf::Sprite player(playerTexture);
+    player.setPosition(400, 300); 
+//propotionally increase the scale of sprite
+    player.setScale(50.0 / playerTexture.getSize().x, 50.0 / playerTexture.getSize().y);
+
+    sf::Texture sunflowerTexture;
+    if (!sunflowerTexture.loadFromFile("./assets/sunflower.png")) {
         return -1;
     }
-    bubbleTexture.setSmooth(true);
-    sf::Sprite bubble(bubbleTexture);
-    bubble.setScale(0.05f, 0.05f);
+
+    sf::Sprite sunflower(sunflowerTexture);
+
+
+//propotionally increase the scale of sprite
+    sunflower.setScale(25.0 / sunflowerTexture.getSize().x, 25.0 / sunflowerTexture.getSize().y);
 
     std::srand(std::time(NULL));
 
-    float x = (float)(std::rand() % (windowSize.x - (int)(bubble.getGlobalBounds().width)));
-    float y = (float)(std::rand() % (windowSize.y - (int)(bubble.getGlobalBounds().height)));
-    bubble.setPosition(x, y);
-    
+    float playerSpeed = 200.0; 
 
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
+    int score = 0;
+
+    sf::Font font;
+    if (!font.loadFromFile("./assets/ARIALBD.ttf")) {
+        return -1;
+    }
+
+    sf::Text scoreText;
+    scoreText.setFont(font);
+    scoreText.setCharacterSize(24);
+
+    scoreText.setFillColor(sf::Color::White);
+
+    scoreText.setString("Score: " + std::to_string(score));
+
+    // Center the score text
+
+    scoreText.setPosition(windowSize.x / 2.0, 0); 
+
+    sf::Clock clock;
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
-        }
-            window.clear();
-        auto mouse_pos = sf::Mouse::getPosition(window); // Mouse position relative to the window
-        auto translated_pos = window.mapPixelToCoords(mouse_pos); // Mouse position translated into world coordinates
-        if(bubble.getGlobalBounds().contains(translated_pos)){ // Rectangle-contains-point check
-            // Mouse is inside the sprite.
-                float x = (float)(std::rand() % (windowSize.x - (int)(bubble.getGlobalBounds().width)));
-                float y = (float)(std::rand() % (windowSize.y - (int)(bubble.getGlobalBounds().height)));
-                    sound.play();
-                bubble.setPosition(x, y);
             }
-        // Draw the background
-    window.draw(background);
-    window.draw(bubble);
-    window.display();
+        }
 
-    }        
+        sf::Time deltaTime = clock.restart();
+        float dt = deltaTime.asSeconds();
+    sf::Vector2 playerPos = player.getPosition();
+        sf::Text exitButtonText;
+        exitButtonText.setFont(font);
+    exitButtonText.setCharacterSize(24);
+         exitButtonText.setFillColor(sf::Color::White);
+        exitButtonText.setString("Press esc to Exit");
+        exitButtonText.setPosition(windowSize.x/2 - 100 ,30);
+
+        sf::Vector2 movement(0.0f, 0.0f);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            movement.x -= playerSpeed * dt;
+            player.setRotation(270); 
+        }
+        if(playerPos.x > 800 || playerPos.y > 600 || playerPos.x < 0 || playerPos.y < 0 ) {
+            window.close();
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            movement.x += playerSpeed * dt;
+            player.setRotation(90); 
+        }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            movement.y -= playerSpeed * dt;
+            player.setRotation(0); 
+        }
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            movement.y += playerSpeed * dt;
+            player.setRotation(180); 
+        }
+        
+        
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+            window.close();
+        }
+        if (movement.x != 0 && movement.y != 0) {
+            float angle = std::atan2(movement.y, movement.x) * 180 / 3.14159265;
+
+            player.setRotation(angle + 90); 
+        }
+
+        player.move(movement);
+
+        if (player.getGlobalBounds().intersects(sunflower.getGlobalBounds())) {
+            score++;
+            scoreText.setString("Score: " + std::to_string(score));
+
+
+            scoreText.setPosition(windowSize.x / 2.0, 0); 
+
+            float x = (float)(std::rand() % (windowSize.x - (int)(sunflower.getGlobalBounds().width)));
+            float y = (float)(std::rand() % (windowSize.y - (int)(sunflower.getGlobalBounds().height)));
+            sunflower.setPosition(x, y);
+        }
+        
+
+        window.clear();
+        window.draw(background);
+        window.draw(sunflower);
+        window.draw(player);
+        window.draw(scoreText);
+        window.draw(exitButtonText);
+        window.display();
+    }
 
     return 0;
 }
